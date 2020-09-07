ff2ffd67ef0c021bde071ac79156103268c01d71
probison <parker.robison@hotmail.com>
2020-09-06 19:32:41 -0600

    fixed atlas issue

--- server.js
+++ server.js
@@ -2,10 +2,11 @@ const express = require("express");
 const logger = require("morgan");
 const mongoose = require("mongoose");
 const compression = require("compression");
+require("dotenv").config()
 
 const PORT = process.env.PORT || 3001;
-const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/budget";
-
+// const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/budget";
+const MONGODB_URI = `mongodb+srv://parkerr:${process.env.DB_PASS}@budget.5t8xc.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`
 const app = express();
 
 app.use(logger("dev"));
